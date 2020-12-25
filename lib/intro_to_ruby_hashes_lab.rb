f7d829c075ea0dd2887e30737381aafe9f26ac66
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:58:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,9 +5,9 @@ def my_hash_creator(key, value)
   
 end
 
-# def read_from_hash(hash, key)
-#   hash[key]
-# end
+def read_from_hash(hash, key)
+  hash[key]
+end
 
 # def update_counting_hash(hash, key)
 #   if hash[key]
