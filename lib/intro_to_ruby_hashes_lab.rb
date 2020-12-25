a2014d0834268ba9e3d13a7ff59cbdb89e279c5c
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:50:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,12 @@
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
   myHash = {
-    :key => value
+    key => value
   }
+  myHash
   
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
   hash[key]
 end
 
