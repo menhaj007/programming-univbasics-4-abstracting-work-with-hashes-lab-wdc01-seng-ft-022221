06f6964e0122fa8445d0939c3a385612c4273cc6
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:56:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,14 +5,14 @@ def my_hash_creator(key, value)
   
 end
 
-def read_from_hash(hash, key)
-  hash[key]
-end
+# def read_from_hash(hash, key)
+#   hash[key]
+# end
 
-def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] = hash[hash] + 1
-  else
-    hash[key] = 1
-  end
-end
+# def update_counting_hash(hash, key)
+#   if hash[key]
+#     hash[key] = hash[hash] + 1
+#   else
+#     hash[key] = 1
+#   end
+# end
