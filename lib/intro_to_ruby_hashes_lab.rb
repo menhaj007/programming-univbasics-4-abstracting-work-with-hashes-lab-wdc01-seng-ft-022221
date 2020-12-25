17c1c22bb638765d794d65a132894723aa521c85
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:56:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,16 @@ end
 #   end
 # end
 
+# def update_counting_hash(hash, key)
+#   hash[key] ? hash[key] += 1 : hash[key] = 1
+#   hash
+# end
+
 def update_counting_hash(hash, key)
-  hash[key] ? hash[key] += 1 : hash[key] = 1
+  if hash[key]
+    hash[key] = hash[key] + 1
+  else
+    hash[key] = 1
+  end
   hash
 end
